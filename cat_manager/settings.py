d9bfb9f49bf8233b7612589b95fa57aced82c11e
Fraiters <dutovmv@mail.ru>
2023-11-27 23:49:06 +0300

    Исправляю параметры доступа к домену

--- cat_manager/settings.py
+++ cat_manager/settings.py
@@ -24,7 +24,9 @@ SECRET_KEY = 'django-insecure-(g7(ezf7%(aom=-ktemp#r1jfrs218f8f1ef@)bp_*d&=a!d*v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['catmanager-production.up.railway.app/', '127.0.0.1']
+ALLOWED_HOSTS = ['catmanager-production.up.railway.app', '127.0.0.1']
+
+CSRF_TRUSTED_ORIGINS = ['https://catmanager-production.up.railway.app']
 
 # Application definition
 
@@ -115,8 +117,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-CSRF_TRUSTED_ORIGINS = ['https://catmanager-production.up.railway.app/']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
